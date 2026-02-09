4d4ab5e7fd6d4c3e96ad816f63531569a3608ced
esoo12 <esraamohsan239@gmail.com>
2026-02-09 16:01:55 +0200

    add ClientUsermodel and NurseUserModel classes; update RequestModel to include user and nurse fields

---
+++ lib/core/models/client_user_model.dart
@@ -0,0 +1,7 @@
+class ClientUsermodel {
+  String name;
+  DateTime dateOfBirth;
+  String phone;
+  String email;
+  ClientUsermodel({required this.dateOfBirth,required this.email,required this.name,required this.phone});
+}
---
+++ lib/core/models/nurse_user_model.dart
@@ -0,0 +1,12 @@
+class NurseUserModel {
+  String name;
+  DateTime dateOfBirth;
+  String phone;
+  String email;
+  String location;
+  int experience;
+  NurseUserModel({required this.dateOfBirth,
+  required this.email,
+  required this.name,
+  required this.phone,required this.experience,required this.location});
+}
